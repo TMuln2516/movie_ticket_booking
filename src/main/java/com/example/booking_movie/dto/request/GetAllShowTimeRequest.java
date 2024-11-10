2cdfe989f9c8c0800ea86ada1adb54d0469cec76
TMuln2516 <qhuynehihi@gmail.com>
2024-11-10 11:17:44 +0700

    update GetAllShowTimeRequest

--- src/main/java/com/example/booking_movie/dto/request/GetAllShowTimeRequest.java
+++ src/main/java/com/example/booking_movie/dto/request/GetAllShowTimeRequest.java
@@ -1,11 +1,13 @@
 package com.example.booking_movie.dto.request;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Set;
 
 @Data
@@ -13,34 +15,8 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class CreateMovieRequest {
-    @NotNull(message = "Name must not be null")
-    String name;
-
-    @NotNull(message = "Premiere Day must not be null")
-    @Future(message = "Invalid date")
-    LocalDate premiere;
-
-    @NotNull(message = "Language must not be null")
-    String language;
-
-    @Min(value = 60, message = "Duration must be at least 60 minutes")
-    Integer duration;
-
-    @NotNull(message = "Content must not be null")
-    String content;
-
-    Double rate;
-
-    @NotNull(message = "Image must not be null")
-    String image;
-
-    @NotEmpty
-    Set<String> genresId;
-
-    @NotNull
-    String directorId;
-
-    @NotEmpty
-    Set<String> actorsId;
+public class GetAllShowTimeRequest {
+    String movieId;
+    LocalDate date;
+    String location;
 }
