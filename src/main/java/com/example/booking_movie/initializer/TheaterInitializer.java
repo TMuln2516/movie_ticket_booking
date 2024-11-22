e9a8b70d1aec53f5b87a383ffff70d17eba9a5d5
TMuln2516 <qhuynehihi@gmail.com>
2024-11-22 09:02:34 +0700

    update

--- src/main/java/com/example/booking_movie/initializer/TheaterInitializer.java
+++ src/main/java/com/example/booking_movie/initializer/TheaterInitializer.java
@@ -14,35 +14,35 @@ public class TheaterInitializer {
     TheaterRepository theaterRepository;
 
     public void initializeTheaters() {
-        if (theaterRepository.existsByNameAndLocation("Galaxy Linh Trung Thủ Đức", "Hồ Chí Minh")) {
+        if (!theaterRepository.existsByNameAndLocation("Galaxy Linh Trung Thủ Đức", "Hồ Chí Minh")) {
             theaterRepository.save(Theater.builder()
                             .name("Galaxy Linh Trung Thủ Đức")
                             .location("Hồ Chí Minh")
                     .build());
         }
 
-        if (theaterRepository.existsByNameAndLocation("CGV Vincom Thủ Đức", "Hồ Chí Minh")) {
+        if (!theaterRepository.existsByNameAndLocation("CGV Vincom Thủ Đức", "Hồ Chí Minh")) {
             theaterRepository.save(Theater.builder()
                     .name("CGV Vincom Thủ Đức")
                     .location("Hồ Chí Minh")
                     .build());
         }
 
-        if (theaterRepository.existsByNameAndLocation("CGV Giga Mall Thủ Đức", "Hồ Chí Minh")) {
+        if (!theaterRepository.existsByNameAndLocation("CGV Giga Mall Thủ Đức", "Hồ Chí Minh")) {
             theaterRepository.save(Theater.builder()
                     .name("CGV Giga Mall Thủ Đức")
                     .location("Hồ Chí Minh")
                     .build());
         }
 
-        if (theaterRepository.existsByNameAndLocation("CGV Vincom Đà Nẵng", "Đà Nẵng")) {
+        if (!theaterRepository.existsByNameAndLocation("CGV Vincom Đà Nẵng", "Đà Nẵng")) {
             theaterRepository.save(Theater.builder()
                     .name("CGV Vincom Đà Nẵng")
                     .location("Đà Nẵng")
                     .build());
         }
 
-        if (theaterRepository.existsByNameAndLocation("CGV Vincom Bà Triệu", "Hà Nội")) {
+        if (!theaterRepository.existsByNameAndLocation("CGV Vincom Bà Triệu", "Hà Nội")) {
             theaterRepository.save(Theater.builder()
                     .name("CGV Vincom Bà Triệu")
                     .location("Hà Nội")
